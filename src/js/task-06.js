8b06901cbddf531414c306d1a23922ee36e02ff0
Marek Rożko <maknamar12@gmail.com>
2022-11-01 22:15:51 +0100

    add task 6

---
+++ src/js/task-06.js
@@ -0,0 +1,15 @@
+
+const textInput = document.querySelector("input");
+const correctValue = Number(textInput.dataset.length);
+
+textInput.addEventListener('blur', checkInputValue);
+
+function checkInputValue() {
+    const { value, classList } = textInput;
+    if (value.trim().length !== correctValue) {
+        this.classList.add('invalid');
+    } else {
+        classList.remove('invalid');
+        classList.add('valid');
+    }
+}
