40a0e7dbafcbfc028eb3930a8e1e401850c9cb94
Marek Rożko <maknamar12@gmail.com>
2022-11-06 13:54:09 +0100

    add task 8,9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const windowBgColor = document.body;
+const btnEl = document.querySelector(".change-color")
+const colorEl = windowBgColor.querySelector(".color");
+
+btnEl.addEventListener('click', changeBgColor)
+
+function changeBgColor(event) {
+  if (event) {
+    colorEl.textContent = getRandomHexColor();
+
+    windowBgColor.style.backgroundColor = colorEl.textContent;
+  }
+}
+
+
+
